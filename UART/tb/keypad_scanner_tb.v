7c7f2ee78644e2a2ce8512cd227583fdee62d10b
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-08 18:40:57 +0800

    Update keypad_scanner_tb.v

--- UART/tb/keypad_scanner_tb.v
+++ UART/tb/keypad_scanner_tb.v
@@ -7,6 +7,8 @@ module keypad_scanner_tb;
     wire [3:0] C;
     wire key_valid;
     wire [3:0] key;
+    
+    integer i;
 
     keypad_scanner DUT (
         .clk(clk),
@@ -17,7 +19,7 @@ module keypad_scanner_tb;
         .key(key)
     );
 
-    defparam DUT.DEBOUNCE_INST.DEBOUNCE_TIME = 20;
+    defparam DUT.DEBOUNCE_INST.DEBOUNCE_TIME = 50;
 
     always #10 clk = ~clk;
 
@@ -28,36 +30,52 @@ module keypad_scanner_tb;
         repeat (5) @(posedge clk);
         reset = 0;
 
-        //Press key '1' (Column 0, Row 0)
+        //Short press key '1'  (Column 0, Row 0)
         wait(C == 4'b1110);
         @(posedge clk);
         R = 4'b1110;   
-        repeat (80) @(posedge clk);
+        wait(C == 4'b1101);
         R = 4'b1111;
         repeat (50) @(posedge clk);
 
         // Press key '5' (Column 1, Row 1)
-        wait(C == 4'b1101);
-        @(posedge clk);
-        R = 4'b1101; 
-        repeat (80) @(posedge clk);
-        R = 4'b1111;
+        for(i = 0; i<=DUT.DEBOUNCE_INST.DEBOUNCE_TIME/20; i = i+1) begin
+          wait(C == 4'b1101);
+          @(posedge clk);
+          R = 4'b1101; 
+          wait(C == 4'b1011);
+          R = 4'b1111;
+        end
+        repeat (50) @(posedge clk);
+        
+        // Press key '5' again (Column 1, Row 1)
+        for(i = 0; i<=DUT.DEBOUNCE_INST.DEBOUNCE_TIME/20; i = i+1) begin
+          wait(C == 4'b1101);
+          @(posedge clk);
+          R = 4'b1101; 
+          wait(C == 4'b1011);
+          R = 4'b1111;
+        end
         repeat (50) @(posedge clk);
 
         // Press key '9' (Column 2, Row 2)
-        wait(C == 4'b1011);
-        @(posedge clk);
-        R = 4'b1011;            
-        repeat (80) @(posedge clk);
-        R = 4'b1111;
+        for(i = 0; i<=DUT.DEBOUNCE_INST.DEBOUNCE_TIME/20; i = i+1) begin
+          wait(C == 4'b1011);
+          @(posedge clk);
+          R = 4'b1011; 
+          wait(C == 4'b0111);
+          R = 4'b1111;
+        end
         repeat (50) @(posedge clk);
 
         // Press key 'D' (Column 3, Row 3)
-        wait(C == 4'b0111);
-        @(posedge clk);
-        R = 4'b0111;            
-        repeat (80) @(posedge clk);
-        R = 4'b1111;
+        for(i = 0; i<=DUT.DEBOUNCE_INST.DEBOUNCE_TIME/20; i = i+1) begin
+          wait(C == 4'b0111);
+          @(posedge clk);
+          R = 4'b0111; 
+          wait(C == 4'b1110);
+          R = 4'b1111;
+        end
         repeat (50) @(posedge clk);
 
         $stop;
