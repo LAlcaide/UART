3ea552bebd5b8a58def064d87162ea3331bec7d8
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-10 12:05:34 +0800

    Update baud_generator.v

--- UART/rtl/baud_generator.v
+++ UART/rtl/baud_generator.v
@@ -1,5 +1,5 @@
 `default_nettype none
-module baud_generator(
+module baud_generator #(parameter baud_9600 = 9'd326, baud_19200 = 9'd163, baud_38400 = 9'd81, baud_57600 = 9'd54, baud_115200 = 9'd27)(
 input wire clk, reset, 
 input wire [2:0] baud_value,
 output reg baud_tick
@@ -9,12 +9,12 @@ output reg baud_tick
   //Divider lookup
   always @(*) begin
     case(baud_value)
-        3'd0: divider = 9'd326; // 9600 baud
-        3'd1: divider = 9'd163; // 19200 baud
-        3'd2: divider = 9'd81; // 38400 baud
-        3'd3: divider = 9'd54; // 57600 baud
-        3'd4: divider = 9'd27; // 115200 baud
-        default: divider = 9'd326;
+        3'd0: divider = baud_9600;
+        3'd1: divider = baud_19200;
+        3'd2: divider = baud_38400;
+        3'd3: divider = baud_57600;
+        3'd4: divider = baud_115200;
+        default: divider = baud_9600;
     endcase
   end
   
@@ -29,7 +29,7 @@ output reg baud_tick
       end
       else begin
         baud_tick<=0;
-        tick_counter<=tick_counter + 1; 
+        tick_counter<=tick_counter + 9'd1; 
       end
     end
   end
